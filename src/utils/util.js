39e9b0a74f051cb188258d06b97e5f68d5f753b6
eileendchen <eileendchen@gmail.com>
2021-03-28 11:34:53 -0700

    Updates

--- src/utils/util.js
+++
@@ -1,8 +0,0 @@
-const PLANT_DATA = {
-    "tomato": {
-        scientific_name: "Solanum lycopersicum",
-        watering_frequency: 2 // number of times per week
-    }
-};
-
-export default PLANT_DATA;
